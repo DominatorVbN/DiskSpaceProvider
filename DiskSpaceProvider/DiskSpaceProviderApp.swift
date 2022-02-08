ccb0e77d8f9b3d108967f3596547ad1e9cf79954
Amit Samant <amit.samant@gojek.com>
2022-02-08 22:18:13 +0530

    Initial Commit

---
+++ DiskSpaceProvider/DiskSpaceProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskSpaceProviderApp.swift
+//  DiskSpaceProvider
+//
+//  Created by Amit Samant on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DiskSpaceProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
